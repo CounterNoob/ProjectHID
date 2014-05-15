71dc75856deefe73f7c5213e60c2296ac2156aa6
Kristian Andersen <kris101b@edu.eal.dk>
2014-05-15 10:53:22 +0200

    Added database commit to try except

--- HID.py
+++ HID.py
@@ -13,19 +13,21 @@ def main():
 	except:
 		pass
 	print("Starting measurements...")
-	while True:
-		temp = randint(15,30)#sensortag.getAmbTemp()
-		humi = randint(30,80)#sensortag.getHumid()
-		systime = datetime.datetime.now()
-		conn.execute("INSERT INTO history VALUES (?, ?, ?)", (systime, temp, humi))
-		print("\nStored history readings:\nTime:", systime,\
-			"\nTemperature:", temp, "\nHumidity:", humi)
-		for x in range(1,30):
-			temp1 = randint(15,30)#sensortag.getAmbTemp()
-			humi1 = randint(30,80)#sensortag.getHumid()
-			conn.execute("""UPDATE current SET temp = ?, humi = ?  WHERE now = 1""", (temp1, humi1))
-			print("\nStored current readings:\nTemperature:", temp1, "\nHumidity:", humi1)
-			#time.sleep(60)
-			time.sleep(5)
-
+	try:
+		while True:
+			temp = randint(15,30)#sensortag.getAmbTemp()
+			humi = randint(30,80)#sensortag.getHumid()
+			systime = datetime.datetime.now()
+			conn.execute("INSERT INTO history VALUES (?, ?, ?)", (systime, temp, humi))
+			print("\nStored history readings:\nTime:", systime,\
+				"\nTemperature:", temp, "\nHumidity:", humi)
+			for x in range(1,30):
+				temp1 = randint(15,30)#sensortag.getAmbTemp()
+				humi1 = randint(30,80)#sensortag.getHumid()
+				conn.execute("""UPDATE current SET temp = ?, humi = ?  WHERE now = 1""", (temp1, humi1))
+				print("\nStored current readings:\nTemperature:", temp1, "\nHumidity:", humi1)
+				#time.sleep(60)
+				time.sleep(5)
+	except:
+		conn.commit()
 main()
